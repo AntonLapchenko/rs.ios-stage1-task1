10d0c7b518947b11e8999007bd7c01937acd9e7d
Anton <tatyana.shut.solveit@gmail.com>
2020-03-14 11:32:19 +0300

    feat: add Task 4

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,7 +4,42 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    NSMutableArray *resultArray = [NSMutableArray new];
-    return @[@"Test"];
+    
+    NSArray *openBrackets = [NSArray arrayWithObjects:@"<",@"[",@"(", nil];
+    NSArray *closeBrackets = [NSArray arrayWithObjects:@">",@"]",@")", nil];
+    NSMutableArray<NSString *> *resultArray = [NSMutableArray new];
+    NSUInteger repeat = 0;
+    
+    for (NSInteger letter = 0; letter < [string length]; letter++) {
+        for (NSInteger bracket = 0; bracket < openBrackets.count; bracket++) {
+            if ([NSString stringWithFormat:@"%C",[string characterAtIndex: letter]] ==
+                [NSString stringWithFormat:@"%@",[openBrackets objectAtIndex:bracket]]) {
+                
+                for (NSInteger i = letter + 1; i < [string length]; i++) {
+                    
+                    if ([NSString stringWithFormat:@"%C",[string characterAtIndex: i]] ==
+                        [NSString stringWithFormat:@"%@",[openBrackets objectAtIndex: bracket]]) {
+                        repeat++;
+                        continue;
+                    }
+                    
+                    if ([NSString stringWithFormat:@"%C",[string characterAtIndex: i]] ==
+                        [NSString stringWithFormat:@"%@",[closeBrackets objectAtIndex: bracket]] && repeat != 0) {
+                        repeat--;
+                        continue;
+                    }
+                    
+                    if ([NSString stringWithFormat:@"%C",[string characterAtIndex: i]] ==
+                        [NSString stringWithFormat:@"%@",[closeBrackets objectAtIndex: bracket]] && repeat == 0) {
+                        [resultArray addObject:[string substringWithRange: NSMakeRange(letter + 1, i - letter - 1)]];
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    
+    return resultArray;
 }
+
 @end
